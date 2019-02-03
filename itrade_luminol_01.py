c037d7d38c5ab59e2abf3473be7f45552abe4e6e
Kedwin Chen <kedwinchen@gmail.com>
2019-02-02 22:28:35 -0800

    20190202 itrade LUMINOL_02 Exclude extreme outliers
    Successfully able to plot the data of `DELIVERED_PRICE` over time, as
    weekly values.
    
    Currently, the code drops extreme outliers without saving them.

--- itrade_luminol_01.py
+++ itrade_luminol_01.py
@@ -12,11 +12,12 @@ from matplotlib import dates
 
 def ingest_csv(fpath):
     # Dateformat: 01-Mar-17 (%d-%b-%y)
-    dateparse = lambda x: (pd.to_datetime(pd.datetime.strptime(x, '%d-%b-%y')) - datetime(1970, 1, 1)).total_seconds()
+    #dateparse = lambda x: (pd.to_datetime(pd.datetime.strptime(x, '%d-%b-%y')) - datetime(1970, 1, 1)).total_seconds()
+    dateparse = lambda x: pd.to_datetime(pd.datetime.strptime(x, '%d-%b-%y'))
     # nrows=10000
-    # df = pd.read_csv(fpath, sep=',', parse_dates=['TRANSACTION_DATE'], date_parser=dateparse)
-    df = pd.read_csv(fpath, sep=',')
-    df['TRANSACTION_DATE'] = [int(dateparse(x)) for x in df['TRANSACTION_DATE']]
+    df = pd.read_csv(fpath, sep=',', parse_dates=['TRANSACTION_DATE'], date_parser=dateparse).set_index('TRANSACTION_DATE')
+    #df = pd.read_csv(fpath, sep=',')
+    #df['TRANSACTION_DATE'] = [int(dateparse(x)) for x in df['TRANSACTION_DATE']]
     return df
 
 def plot_anomalies(ts, anomalies):
@@ -27,7 +28,8 @@ def plot_anomalies(ts, anomalies):
     fig, ax1 = pyplot.subplots()
 
     # Put data on graph
-    pyplot.plot(x_ts_dates, ts.values())
+    #pyplot.plot(x_ts_dates, ts.values())
+    pyplot.scatter(x_ts_dates, ts.values())
     pyplot.scatter(x_anomalies_dates, anomalies.values(), c='red')
 
     # Set axis to be labeled by dates
@@ -37,18 +39,26 @@ def plot_anomalies(ts, anomalies):
     pyplot.show()
 
 def main():
-    FILE = 'anomaly-5k.csv'
-    # FILE = 'Anomaly_Data_NO-CR.csv'
+    #FILE = 'anomaly-5k.csv'
+    FILE = 'Anomaly_Data_NO-CR.csv'
     df = ingest_csv(FILE)
     #ts = {}
-    group_tdate = df.groupby(['TRANSACTION_DATE'])[ ['DELIVERED_PRICE'] ].sum().apply(list).to_dict()[ 'DELIVERED_PRICE' ]
+    #group_tdate = df.groupby(['TRANSACTION_DATE'])[ ['DELIVERED_PRICE'] ].sum().apply(list).to_dict()[ 'DELIVERED_PRICE' ]
+    group_tdate = df.groupby( pd.Grouper(freq='W') )[ ['DELIVERED_PRICE'] ].sum().apply(list).to_dict()[ 'DELIVERED_PRICE' ]
     #keys = list( ts['TRANSACTION_DATE'] )
-    keys = list( group_tdate.keys() )
+    #keys = list( (group_tdate.keys() - datetime(1970, 1, 1)).total_seconds()
+    keys = [  int( (x - datetime(1970, 1, 1)).total_seconds()) for x in list(group_tdate.keys()) ]
     #values = list( ts['DELIVERED_PRICE'])
     values = list( group_tdate.values() )
 
     ts = dict(zip(keys, values))
     #ts = df['DELIVERED_PRICE'].groupby(df['TRANSACTION_DATE']).sum()
+
+    # In case we need it laters
+    # extreme_anomalies = []
+    # 400000 is a magic number that is a lower limit for the extreme outliers
+    # Remove this; is not helpful for our modeling of other outliers
+    ts = { k:v for k,v in ts.items() if v < 400000 }
 
     print("keys: ", len(keys))
     print("values: ", len(values))
@@ -59,11 +69,12 @@ def main():
 
     detector = AnomalyDetector( time_series=ts,
                                 #algorithm_name='derivative_detector',
-                                algorithm_name='exp_avg_detector',
-                                algorithm_params={
-                                        'smoothing_factor' : 0.05,
-                                        'use_lag_window' : True
-                                })
+                                #algorithm_name='exp_avg_detector',
+                                #algorithm_params={
+                                #        'smoothing_factor' : 0.05,
+                                #        'use_lag_window' : True
+                                #}
+                                )
     score = detector.get_all_scores()
 
     n_anomaly = 0
